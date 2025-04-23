76967bfbd2ab86fac8cf3470fb87fcfe1c5266c8
Samy Mizi Allaoua <2393410@cmontmorency.qc.ca>
2025-04-22 20:52:43 -0400

    test

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from model import platine
-from model import mesure
 from controler import controler
-from view import view
 
-# Initialisation des composants
-platine = platine()
-view = view()
-controleur = controler(platine, view)
-
+controler = controler()
+controler.demarrer()
